2218b1068c0548f503585da4d756224c61c59d87
braulio2611 <81401319+braulio2611@users.noreply.github.com>
2021-03-25 21:54:56 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const texto1=document.getElementById("texto1")
+const texto2=document.getElementById("texto2")
+const texto3=document.getElementById("texto3")
+const texto4=document.getElementById("texto4")
+
+
+function datos1(){
+texto1.innerHTML="Braulio García"
+texto2.innerHTML="Edad: 19"
+texto3.innerHTML="Genero: Masculino"
+texto4.innerHTML="No. Carnét: 000000"
+}
+
+function datos2(){
+    texto1.innerHTML="Instituto Evangñelico America Latina"
+    texto2.innerHTML="Zona 13"
+    texto3.innerHTML="Fundador: Don Virilio Zapáta y Doña Beatriz de Zapáta"
+    texto4.innerHTML="Fundado en 1954"
+    }
+
+    function datos3(){
+        texto1.innerHTML="Universidad Mariano Gálvez"
+        texto2.innerHTML="Zona 2"
+        texto3.innerHTML="2411 1800"
+        texto4.innerHTML="Fundado en 1966"
+        }
+        
+        function datos4(){
+            texto1.innerHTML="Tocar bateria"
+            texto2.innerHTML="Programar"
+            texto3.innerHTML="Escuchar música"
+            texto4.innerHTML="Jugar basquetbol"
+            }
+
